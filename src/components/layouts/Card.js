4415fcd30860c6bda97f715a4dda6ad35c7a15e7
hendraaagil <hendraaagil@gmail.com>
2020-12-19 18:21:51 +0700

    Complete & fix some styling

--- src/components/layouts/Card.js
+++ src/components/layouts/Card.js
@@ -4,8 +4,10 @@ const Card = ({ title, imgSrc }) => {
   return (
     <div className={styles.Card}>
       <img src={imgSrc} alt={`${title} Illustration`} />
-      <h3>{title}</h3>
-      <p>See More ›</p>
+      <div>
+        <h3>{title}</h3>
+        <p>See More ›</p>
+      </div>
     </div>
   );
 };
